dfc4060002a28e1f055fe448f5e0ef9aa910cdbb
Edward <edward_song@yeah.net>
2017-04-07 20:15:01 +0800

    Create ini

--- BarcodeScan/p.cs
+++ BarcodeScan/p.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Windows.Forms ;
 using System.IO.Ports;
+using Edward;
 
 namespace BarcodeScan
 {
@@ -37,8 +38,43 @@ namespace BarcodeScan
         public static string Close_Scan_Command = string.Empty;
         public static bool Open_Add_Enter = true;
         public static bool Close_Add_Enter = true;
-     
 
+
+
+       public enum IniSection
+        {
+            SysConfig,
+            PLC_COM,
+            Bar_COM,
+        }
+
+
+        /// <summary>
+        /// create ini file,write the default value
+        /// </summary>
+        /// <param name="inifilepath">ini path</param>
+        public static void createIniFile(string inifilepath)
+        {
+            IniFile.CreateIniFile(inifilepath);
+            //
+            IniFile.IniWriteValue(IniSection.PLC_COM .ToString (), "PLC_Port", PLC_Port);
+            IniFile.IniWriteValue(IniSection.PLC_COM.ToString(), "PLC_Baud_Rate", PLC_Baud_Rate);
+            IniFile.IniWriteValue(IniSection.PLC_COM.ToString(), "PLC_Data_Bits", PLC_Data_Bits.ToString());
+            IniFile.IniWriteValue(IniSection.PLC_COM.ToString(), "PLC_Stop_Bits", PLC_Stop_Bits.ToString());
+            IniFile.IniWriteValue(IniSection.PLC_COM.ToString(), "PLC_Parity", PLC_Parity.ToString());
+            //
+            IniFile.IniWriteValue(IniSection.Bar_COM.ToString(), "Scan_A_Port", Scan_A_Port);
+            IniFile.IniWriteValue(IniSection.Bar_COM.ToString(), "Scan_B_Port", Scan_B_Port);
+            IniFile.IniWriteValue(IniSection.Bar_COM.ToString(), "Scan_C_Port", Scan_C_Port);
+            IniFile.IniWriteValue(IniSection.Bar_COM.ToString(), "Scan_D_Port", Scan_D_Port);
+            IniFile.IniWriteValue(IniSection.Bar_COM.ToString(), "Scan_Baud_Rate", Scan_Baud_Rate);
+            IniFile.IniWriteValue(IniSection.Bar_COM.ToString(), "Scan_Data_Bits", Scan_Data_Bits.ToString());
+            IniFile.IniWriteValue(IniSection.Bar_COM.ToString(), "Scan_Stop_Bits", Scan_Stop_Bits.ToString());
+            IniFile.IniWriteValue(IniSection.Bar_COM.ToString(), "Scan_Parity", Scan_Parity.ToString());
+            //
+            IniFile.IniWriteValue(IniSection.Bar_COM.ToString(), "Open_Add_Enter", Open_Add_Enter.ToString());
+            IniFile.IniWriteValue(IniSection.Bar_COM.ToString(), "Close_Add_Enter", Close_Add_Enter.ToString());
+        }
 
 
 
