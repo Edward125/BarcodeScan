70bc4a4ea46ec1292858f56471b3a3381bdadfcf
Edward <edward_song@yeah.net>
2017-04-07 19:47:57 +0800

    main ui ok

--- BarcodeScan/frmMain.Designer.cs
+++ BarcodeScan/frmMain.Designer.cs
@@ -44,27 +44,27 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.comboBarD = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.brnRefresh = new System.Windows.Forms.Button();
+            this.txtBarA = new System.Windows.Forms.TextBox();
+            this.txtBarB = new System.Windows.Forms.TextBox();
+            this.txtBarC = new System.Windows.Forms.TextBox();
+            this.txtBarD = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.groupBox5 = new System.Windows.Forms.GroupBox();
-            this.button5 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.grbTestBarocdeScanner = new System.Windows.Forms.GroupBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.comboBar = new System.Windows.Forms.ComboBox();
             this.grbmessage.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
             this.groupBox4.SuspendLayout();
-            this.groupBox5.SuspendLayout();
+            this.grbTestBarocdeScanner.SuspendLayout();
             this.SuspendLayout();
             // 
             // grbmessage
@@ -88,7 +88,7 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.brnRefresh);
             this.groupBox1.Controls.Add(this.comboBarD);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.label4);
@@ -162,10 +162,10 @@
             this.groupBox2.Controls.Add(this.label8);
             this.groupBox2.Controls.Add(this.label7);
             this.groupBox2.Controls.Add(this.label6);
-            this.groupBox2.Controls.Add(this.textBox4);
-            this.groupBox2.Controls.Add(this.textBox3);
-            this.groupBox2.Controls.Add(this.textBox2);
-            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.txtBarD);
+            this.groupBox2.Controls.Add(this.txtBarC);
+            this.groupBox2.Controls.Add(this.txtBarB);
+            this.groupBox2.Controls.Add(this.txtBarA);
             this.groupBox2.Location = new System.Drawing.Point(258, 12);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(359, 130);
@@ -227,42 +227,50 @@
             this.comboBarD.Size = new System.Drawing.Size(64, 22);
             this.comboBarD.TabIndex = 9;
             // 
-            // button1
+            // brnRefresh
             // 
-            this.button1.Location = new System.Drawing.Point(126, 94);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(101, 28);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.brnRefresh.Location = new System.Drawing.Point(126, 94);
+            this.brnRefresh.Name = "brnRefresh";
+            this.brnRefresh.Size = new System.Drawing.Size(101, 28);
+            this.brnRefresh.TabIndex = 3;
+            this.brnRefresh.Text = "Refresh";
+            this.brnRefresh.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // txtBarA
             // 
-            this.textBox1.Location = new System.Drawing.Point(52, 19);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(298, 22);
-            this.textBox1.TabIndex = 0;
+            this.txtBarA.Location = new System.Drawing.Point(52, 19);
+            this.txtBarA.Name = "txtBarA";
+            this.txtBarA.ReadOnly = true;
+            this.txtBarA.Size = new System.Drawing.Size(298, 22);
+            this.txtBarA.TabIndex = 0;
+            this.txtBarA.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // textBox2
+            // txtBarB
             // 
-            this.textBox2.Location = new System.Drawing.Point(52, 46);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(298, 22);
-            this.textBox2.TabIndex = 1;
+            this.txtBarB.Location = new System.Drawing.Point(52, 46);
+            this.txtBarB.Name = "txtBarB";
+            this.txtBarB.ReadOnly = true;
+            this.txtBarB.Size = new System.Drawing.Size(298, 22);
+            this.txtBarB.TabIndex = 1;
+            this.txtBarB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // textBox3
+            // txtBarC
             // 
-            this.textBox3.Location = new System.Drawing.Point(52, 74);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(298, 22);
-            this.textBox3.TabIndex = 2;
+            this.txtBarC.Location = new System.Drawing.Point(52, 74);
+            this.txtBarC.Name = "txtBarC";
+            this.txtBarC.ReadOnly = true;
+            this.txtBarC.Size = new System.Drawing.Size(298, 22);
+            this.txtBarC.TabIndex = 2;
+            this.txtBarC.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // textBox4
+            // txtBarD
             // 
-            this.textBox4.Location = new System.Drawing.Point(52, 100);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(298, 22);
-            this.textBox4.TabIndex = 3;
+            this.txtBarD.Location = new System.Drawing.Point(52, 100);
+            this.txtBarD.Name = "txtBarD";
+            this.txtBarD.ReadOnly = true;
+            this.txtBarD.Size = new System.Drawing.Size(298, 22);
+            this.txtBarD.TabIndex = 3;
+            this.txtBarD.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label6
             // 
@@ -302,67 +310,72 @@
             // 
             // groupBox4
             // 
-            this.groupBox4.Controls.Add(this.button3);
-            this.groupBox4.Controls.Add(this.button2);
+            this.groupBox4.Controls.Add(this.btnStop);
+            this.groupBox4.Controls.Add(this.btnRun);
             this.groupBox4.Location = new System.Drawing.Point(623, 15);
             this.groupBox4.Name = "groupBox4";
             this.groupBox4.Size = new System.Drawing.Size(192, 62);
             this.groupBox4.TabIndex = 3;
             this.groupBox4.TabStop = false;
-            this.groupBox4.Text = "groupBox4";
             // 
-            // button2
+            // btnRun
             // 
-            this.button2.Location = new System.Drawing.Point(6, 21);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(83, 30);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnRun.Location = new System.Drawing.Point(6, 21);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(83, 30);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnStop
             // 
-            this.button3.Location = new System.Drawing.Point(95, 21);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(78, 30);
-            this.button3.TabIndex = 1;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnStop.Location = new System.Drawing.Point(95, 21);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(78, 30);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
             // 
-            // groupBox5
+            // grbTestBarocdeScanner
             // 
-            this.groupBox5.Controls.Add(this.comboBox1);
-            this.groupBox5.Controls.Add(this.button5);
-            this.groupBox5.Location = new System.Drawing.Point(623, 83);
-            this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Size = new System.Drawing.Size(192, 57);
-            this.groupBox5.TabIndex = 4;
-            this.groupBox5.TabStop = false;
-            this.groupBox5.Text = "groupBox5";
+            this.grbTestBarocdeScanner.Controls.Add(this.comboBar);
+            this.grbTestBarocdeScanner.Controls.Add(this.btnOpen);
+            this.grbTestBarocdeScanner.Location = new System.Drawing.Point(623, 83);
+            this.grbTestBarocdeScanner.Name = "grbTestBarocdeScanner";
+            this.grbTestBarocdeScanner.Size = new System.Drawing.Size(192, 57);
+            this.grbTestBarocdeScanner.TabIndex = 4;
+            this.grbTestBarocdeScanner.TabStop = false;
+            this.grbTestBarocdeScanner.Text = "Test Barcode Scanner";
             // 
-            // button5
+            // btnOpen
             // 
-            this.button5.Location = new System.Drawing.Point(6, 21);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(67, 26);
-            this.button5.TabIndex = 0;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
+            this.btnOpen.Location = new System.Drawing.Point(6, 21);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(67, 26);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
             // 
-            // comboBox1
+            // comboBar
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(95, 25);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(64, 22);
-            this.comboBox1.TabIndex = 10;
+            this.comboBar.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBar.FormattingEnabled = true;
+            this.comboBar.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "D"});
+            this.comboBar.Location = new System.Drawing.Point(95, 25);
+            this.comboBar.Name = "comboBar";
+            this.comboBar.Size = new System.Drawing.Size(64, 22);
+            this.comboBar.TabIndex = 10;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(833, 597);
-            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.grbTestBarocdeScanner);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
@@ -381,7 +394,7 @@
             this.groupBox2.PerformLayout();
             this.groupBox3.ResumeLayout(false);
             this.groupBox4.ResumeLayout(false);
-            this.groupBox5.ResumeLayout(false);
+            this.grbTestBarocdeScanner.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -404,21 +417,21 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.ListBox lstBar;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button brnRefresh;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtBarD;
+        private System.Windows.Forms.TextBox txtBarC;
+        private System.Windows.Forms.TextBox txtBarB;
+        private System.Windows.Forms.TextBox txtBarA;
         private System.Windows.Forms.GroupBox groupBox4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.GroupBox groupBox5;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.GroupBox grbTestBarocdeScanner;
+        private System.Windows.Forms.ComboBox comboBar;
+        private System.Windows.Forms.Button btnOpen;
     }
 }
 
